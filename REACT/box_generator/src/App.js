5c53e413df91e2841203fdd367c31aef5ba98c20
Song Kim <Keyrow223@gmail.com>
2020-05-09 01:45:53 -0700

    First commit for Box Generator Assignment

---
+++ REACT/box_generator/src/App.js
@@ -0,0 +1,29 @@
+import React, { useState } from 'react';
+import './App.css';
+import BoxDisplay from './components/BoxDisplay';
+import BoxForum from './components/BoxForum';
+
+
+function App() {
+
+  const [boxes, setBoxes] = useState([{
+    color: "red",
+    height: "150",
+    style: {
+      display: "inline-block",
+      border: "1px solid red",
+      height: "150px"
+    }
+  }]);
+  
+  return (
+    <div className="App">
+      <h1>Hello, Welcome</h1>
+      <p>Create some colored boxes, add height and color!</p>
+      <BoxForum boxes={boxes} setBoxes={setBoxes} />
+      <BoxDisplay boxes={boxes} />
+    </div>
+  );
+}
+
+export default App;
