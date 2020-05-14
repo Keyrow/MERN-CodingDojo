d00e62b103a719fe92ed20f4465ac816b8702213
Song Kim <Keyrow223@gmail.com>
2020-05-14 14:03:08 -0700

    First commit on Pokemon API assignment

---
+++ REACT/pokemon_api/src/components/Pokemons.js
@@ -0,0 +1,30 @@
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
+
+const Pokemons = props => {
+    const [pokemons, setPokemons] = useState(null);
+
+    useEffect(() => {
+        axios.get('https://pokeapi.co/api/v2/pokemon')
+            .then(res => {
+                console.log("res", res)
+                setPokemons(res.data.results)
+            })
+    }, [])
+
+    return (
+        <>
+            <ul>
+
+                {
+                    pokemons ?
+                        pokemons.map((p, i) =>
+                            <li key={i}>{p.name}</li>
+                        ) : ""
+                }
+            </ul>
+        </>
+    )
+}
+
+export default Pokemons
